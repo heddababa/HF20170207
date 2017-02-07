7425678bdf5dbd9aae3e890ffe4be5fefe7bfc16
Karoly Clement <clement.karoly@gmail.com>
2017-02-07 12:12:47 +0100

    Merge origin/károly into károly
    Conflicts:
    	.gitignore
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/nezet/FoAblak.java
+++ src/nezet/FoAblak.java
@@ -3,8 +3,6 @@ package nezet;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -14,16 +12,14 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
@@ -31,110 +27,113 @@ import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import modell.*;
 
-
-public class FoAblak extends JFrame 
-  implements ActionListener, ListSelectionListener {
+public class FoAblak extends JFrame
+        implements ActionListener {
 
   private JComboBox cbReszlegLista;
-  JLabel lbTalalat=new JLabel("Nincs találat");
-  private JList lDolgozoLista = new JList(new DefaultListModel());
+  JLabel lbTalalat = new JLabel("Nincs találat");
+
   private JTextField tfDolgozoKeres = new JTextField("Keresendő dolgozó", 12);
-  private JScrollPane spDolgozoLista = new JScrollPane(lDolgozoLista);
+
   private JButton btUjDolgozo = new JButton("Új dolgozó felvétele");
   private JLabel lbReszleg = new JLabel("Részleg:   ", SwingConstants.RIGHT);
   private JLabel lbKereses = new JLabel("Dolgozó keresése:   ", SwingConstants.RIGHT);
   private AdatBazisKezeles modell;
-  DefaultListModel dlm = new DefaultListModel();
+  private JTable tDolgozoTable = new JTable(new DefaultTableModel());
+  private JScrollPane spDolgozoLista = new JScrollPane(tDolgozoTable);
 
   public FoAblak(AdatBazisKezeles modell) {
-    this.modell=modell;
+    this.modell = modell;
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     setTitle("HR Fizetésemelés");
     setSize(700, 500);
     setLocationRelativeTo(this);
     setIconImage(Toolkit.getDefaultToolkit().getImage("image1.jpg"));
-    
-    cbReszlegLista = reszlegListaBetoltes();  
-    
-    JPanel pnReszlegek=new JPanel(new GridLayout(1, 7));
+
+    cbReszlegLista = reszlegListaBetoltes();
+
+    JPanel pnReszlegek = new JPanel(new GridLayout(1, 7));
     pnReszlegek.add(lbReszleg);
     pnReszlegek.add(cbReszlegLista);
     pnReszlegek.add(lbKereses);
     tfDolgozoKeres.setText("");
     tfDolgozoKeres.getDocument().addDocumentListener(new MyDocumentListener());
-    tfDolgozoKeres.getDocument().putProperty("name", "Text Area");    
+    tfDolgozoKeres.getDocument().putProperty("name", "Text Area");
     pnReszlegek.add(tfDolgozoKeres);
     lbTalalat.setVisible(false);
     lbTalalat.setForeground(Color.blue);
     lbTalalat.setHorizontalAlignment(SwingConstants.CENTER);
     pnReszlegek.add(lbTalalat);
-    
+
     JPanel pn = new JPanel(new GridLayout(1, 2));
     pn.add(panelKeszit(pnReszlegek));
-    add(pn, BorderLayout.NORTH);    
-//    add(new JPanel(), BorderLayout.LINE_START);
-//    add(new JPanel(), BorderLayout.LINE_END);
-//    add(new JPanel(), BorderLayout.PAGE_END);
-    
+    add(pn, BorderLayout.NORTH);
+
     add(spDolgozoLista);
-    
-    JPanel pnDolgozoGomb = new JPanel(new GridLayout(1,1));
+
+    JPanel pnDolgozoGomb = new JPanel(new GridLayout(1, 1));
     pnDolgozoGomb.add(btUjDolgozo, BorderLayout.CENTER);
     pnDolgozoGomb.setBorder(new EmptyBorder(10, 250, 10, 250));
-    
+
     add(pnDolgozoGomb, BorderLayout.AFTER_LAST_LINE);
-    
+
     setVisible(true);
-    
+
     cbReszlegLista.addActionListener(this);
-    //lDolgozoLista.addListSelectionListener(this);
     btUjDolgozo.addActionListener(this);
-    
-    lDolgozoLista.addMouseListener(new MouseAdapter() {
+
+    tDolgozoTable.addMouseListener(new MouseAdapter() {
       @Override
-      public void mouseClicked(MouseEvent e) {        
-        if (e.getSource()==lDolgozoLista) {
-          Dolgozo dolgozo = (Dolgozo) ((JList) e.getSource()).getSelectedValue();
-          DefaultListModel dlm = (DefaultListModel) lDolgozoLista.getModel();
-          if(dolgozo != null){
-            int dolgozoIndex = dlm.indexOf(dolgozo);
+      public void mouseClicked(MouseEvent e) {
+        if (e.getSource() == tDolgozoTable) {
+          int index = tDolgozoTable.getSelectedRow();
+          MyTableModell tableModell = ((MyTableModell) tDolgozoTable.getModel());
+          Dolgozo dolgozo = tableModell.getDolgozo(index);
+          if (dolgozo != null) {
             new DolgozoFizetesModositas((JFrame) SwingUtilities.getRoot((Component) e.getSource()), dolgozo, modell);
-            //new AdatBekeres(this, dolgozo, modell);
-            dlm.setElementAt(dolgozo, dolgozoIndex);
+            tableModell.steDolgozo(dolgozo, index);
+            tableModell.fireTableDataChanged();
+            tDolgozoTable.addRowSelectionInterval(index, index);
           }
         }
-      }    
+      }
     });
-    
-    lDolgozoLista.addKeyListener(new KeyAdapter() {
+
+    tDolgozoTable.addKeyListener(new KeyAdapter() {
       @Override
-      public void keyReleased(KeyEvent e) {
-        if (e.getSource()==lDolgozoLista)
-          if (e.getKeyCode()==KeyEvent.VK_ENTER)  {
-            Dolgozo dolgozo = (Dolgozo) ((JList) e.getSource()).getSelectedValue();
-            DefaultListModel dlm = (DefaultListModel) lDolgozoLista.getModel();
-            if(dolgozo != null){
-              int dolgozoIndex = dlm.indexOf(dolgozo);
-              //System.out.println(dolgozo.getNev() + " " + dolgozo.getMunkakor());
-              new DolgozoFizetesModositas((JFrame) SwingUtilities.getRoot((JList) e.getSource()), dolgozo, modell);
-              dlm.setElementAt(dolgozo, dolgozoIndex);
+      public void keyPressed(KeyEvent e) {
+        if (e.getSource() == tDolgozoTable) {
+          if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            int index = tDolgozoTable.getSelectedRow();
+            MyTableModell tableModel = ((MyTableModell) tDolgozoTable.getModel());
+            Dolgozo dolgozo = tableModel.getDolgozo(index);
+            if (dolgozo != null) {
+              new DolgozoFizetesModositas((JFrame) SwingUtilities.getRoot((Component) e.getSource()), dolgozo, modell);
+              tableModel.steDolgozo(dolgozo, index);
+              tableModel.fireTableDataChanged();
+              tDolgozoTable.addRowSelectionInterval(index, index);
+              tDolgozoTable.setRowSelectionInterval(index, index);
             }
+          }
         }
-      }  
+      }
     });
+
     reszlegListaBetoltes();
-    lDolgozoLista.setModel(dolgozoListaBetoltes(-1));
-    lDolgozoLista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-    lDolgozoLista.setSelectedIndex(0);
-    lDolgozoLista.requestFocus();
+
+    tDolgozoTable.setModel(dolgozoListaBetoltes(-1));
+    tDolgozoTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    tDolgozoTable.requestFocus();
+    tDolgozoTable.addRowSelectionInterval(0, 0);
   }
-  
+
   private JPanel panelKeszit(JPanel pnReszlegek) {
-    JPanel ujPn=new JPanel();
+    JPanel ujPn = new JPanel();
 //    ujPn.add(new JPanel(), BorderLayout.PAGE_START);
 //    ujPn.add(new JPanel(), BorderLayout.PAGE_END);
 //    ujPn.add(new JPanel(), BorderLayout.LINE_START);
@@ -145,42 +144,36 @@ public class FoAblak extends JFrame
 
   private JComboBox reszlegListaBetoltes() {
     JComboBox cbReszlegLista = new JComboBox();
-    ArrayList<Reszleg> reszlegek=modell.lekerdezReszleg();
+    ArrayList<Reszleg> reszlegek = modell.lekerdezReszleg();
     cbReszlegLista.addItem(new Reszleg(" -- Összes dolgozó", -1));
-    for (Reszleg reszleg : reszlegek)
+    for (Reszleg reszleg : reszlegek) {
       cbReszlegLista.addItem(reszleg);
+    }
     return cbReszlegLista;
   }
 
-  private DefaultListModel dolgozoListaBetoltes(int reszlegID) {
-    dlm = new DefaultListModel();
+  private TableModel dolgozoListaBetoltes(int reszlegID) {
     ArrayList<Dolgozo> dolgozok = modell.lekerdezDolgozokListajaAdottReszleghez(reszlegID);
-    for (Dolgozo dolgozo : dolgozok)
-      dlm.addElement(dolgozo);
-    return dlm;
+    MyTableModell mytm = new MyTableModell(dolgozok);
+    return mytm;
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == cbReszlegLista) {
-      Reszleg reszleg = (Reszleg) ((JComboBox) e.getSource()).getSelectedItem();
-      lDolgozoLista.setModel(dolgozoListaBetoltes(reszleg.getReszlegId()));
+      Reszleg reszleg = (Reszleg) cbReszlegLista.getSelectedItem();
+      tDolgozoTable.setModel(dolgozoListaBetoltes(reszleg.getReszlegId()));
       tfDolgozoKeres.setText("");
       lbTalalat.setVisible(false);
-      lDolgozoLista.setSelectedIndex(0); 
-      lDolgozoLista.requestFocus();
-    } else 
-      new DolgozoFelvetel(this, modell);
-    
+      tDolgozoTable.requestFocus();
+      tDolgozoTable.addRowSelectionInterval(0, 0);
+    }
   }
 
-  public void valueChanged(ListSelectionEvent e) {
-    ;
-  } 
-
   class MyDocumentListener implements DocumentListener {
+
     final String newline = "\n";
-    
+
     public void insertUpdate(DocumentEvent e) {
       updateLog(e);
     }
@@ -190,29 +183,37 @@ public class FoAblak extends JFrame
     }
 
     public void changedUpdate(DocumentEvent e) {
-      ;
+      //Plain text components don't fire these events.
+      updateLog(e);
     }
 
     public void updateLog(DocumentEvent e) {
       if (tfDolgozoKeres.getText().length() > 0) {
         String keres = tfDolgozoKeres.getText().toLowerCase();
-        DefaultListModel dlmSzukitett = new DefaultListModel();
-        for (int i = 0; i < dlm.getSize(); i++) {
-          if (dlm.getElementAt(i).toString().toLowerCase().contains(keres)) {
-            dlmSzukitett.addElement(dlm.getElementAt(i));
+        ArrayList<Dolgozo> dolgozokListaSzükitett = new ArrayList<>();
+        ArrayList<Dolgozo> dolgozoListaAktualis = ((MyTableModell) tDolgozoTable.getModel()).getDolgozLista();
+        for (int i = 0; i < dolgozoListaAktualis.size(); i++) {
+          if (dolgozoListaAktualis.get(i).getNev().toLowerCase().contains(keres)) {
+            dolgozokListaSzükitett.add(dolgozoListaAktualis.get(i));
           }
         }
-        if (dlmSzukitett.size() == 0)
+        if (dolgozokListaSzükitett.size() == 0) {
           lbTalalat.setVisible(true);
-        else
+        } else {
           lbTalalat.setVisible(false);
-        lDolgozoLista.setModel(dlmSzukitett);
-      } 
-      else {
+        }
+        ((MyTableModell) tDolgozoTable.getModel()).setDolgozoLista(dolgozokListaSzükitett);
+        ((MyTableModell) tDolgozoTable.getModel()).fireTableDataChanged();
+        tDolgozoTable.addRowSelectionInterval(0, 0);
+      } else {
         lbTalalat.setVisible(false);
-        lDolgozoLista.setModel(dlm);
+        Reszleg reszleg = (Reszleg) cbReszlegLista.getSelectedItem();
+        tDolgozoTable.setModel(dolgozoListaBetoltes(reszleg.getReszlegId()));
+        tfDolgozoKeres.setText("");
+        lbTalalat.setVisible(false);
+        //tDolgozoTable.requestFocus();
+        tDolgozoTable.addRowSelectionInterval(0, 0);
       }
     }
   }
-  
 }
